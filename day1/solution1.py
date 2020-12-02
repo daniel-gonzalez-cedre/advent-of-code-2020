2fb76efab702b5c3a4c3d0e394f7a92f598ff156
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2020-12-02 14:07:11 -0500

    part 2 solved -- this was super quick but there has to be a better way 	modified:   solution1.py

--- day1/solution1.py
+++ day1/solution1.py
@@ -12,6 +12,16 @@ def part1():
     return
 
 def part2():
+    with open('input.txt') as infile:
+        expenses = [int(line) for line in infile]
+
+    print('part 2:')
+    for i, ei in enumerate(expenses):
+        for j, ej in enumerate(expenses[i:]):
+            for k, ek in enumerate(expenses[j:]):
+                if ei + ej + ek == 2020:
+                    print(f'\t{ei} + {ej} + {ek} = {ei + ej + ek}')
+                    print(f'\t{ei} * {ej} * {ek} = {ei*ej*ek}')
     return
 
 if __name__ == '__main__':
